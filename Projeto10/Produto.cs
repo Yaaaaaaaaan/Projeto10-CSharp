142ccc33c508677b66ea6aa351add1f9b83a85c5
Yan Fonseca <yangoular@gmail.com>
2023-02-27 17:56:52 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto10/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Projeto10
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+    }
+}
